9a2a17ed67bf7147a09a33f34eef5a0f0b23af8e
angel-aristi <angeldaristi@gmail.com>
2025-05-08 21:18:08 -0500

    Se actualiza hasta actividad de cadenas
    Co-authored-by: DanielVilla18 <DanielVilla18@users.noreply.github.com>

--- grupo_1/Variables.py
+++ grupo_1/Variables.py
@@ -1,4 +1,68 @@
+# Ejemplo basico de uso de print
 mensaje = 'Hola equipo'
 print(mensaje)
+
+# Formato de cadenas
+nombre = 'Angel'
+edad = 37
+print(f'Mi nombre es {nombre} y tengo {edad} años')
+
+# Cambiando el caracter al final de la salida
+print('Hola', end=" ")
+print('equipo')
+
+# Especificando un separador diferente
+nombre = 'Daniel'
+edad = 25
+print(nombre, edad, sep='-')
+
+# Ejemplo de declaracion y asignacion de variables
+nombre ='Estefania'
+edad = 25
+altura = 1.75
+es_estudiante = True
+
+# Asignacion de valores en la declaracion
+ciudad = 'Nueva York'
+poblacion = 8500000
+
+# Reasignacion de valores
+ciudad = 'Los Angeles'
+poblacion = 4000000
+
+# Convenciones de nombres
+nombre_completo = 'Jane Doe'
+temperatura_actual = 23.5
+
+# Tipos de variables dinamicos
+
+variable_dinamica = 10
+print(variable_dinamica) # Salida: 10
+
+variable_dinamica = 'Hola'
+print(variable_dinamica) # Salida: Hola
+
+# Tipos de datos basicos
+
+# Enteros
+entero_postivo = 10
+entero_negativo = -5
+
+# Flotantes
+flotante_1 = 3.14
+flotante_2 = -0.5
+
+# Operaciones con enteros y flotantes
+resultado_suma = entero_postivo + flotante_1
+resultado_multiplicacion = entero_negativo * flotante_2
+
+# Impresion de resultados
+print(resultado_suma)
+print(resultado_multiplicacion)
+
+# Cadenas
+cadena_simple = 'Hola, mundo'
+
+
 
 
