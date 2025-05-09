fdbb5150206d7c8651b75dc7961e6569b07016f6
rudalzu <rudalzu@gmail.com>
2025-05-08 20:56:16 -0500

    repaso

---
+++ Grupo 20/variables.py
@@ -0,0 +1,11 @@
+# Formato de cadenas
+nombre = "Rubén"
+edad = 67
+print(f"Mi nombre es {nombre} y tengo {edad} años.")
+# Cambiando el carácter al final de la salida
+print("Hola", end=" ")
+print("mundo!")
+# Especificando un separador diferente
+nombre = "Christian"
+edad = 25
+print(nombre, edad, sep="-")
