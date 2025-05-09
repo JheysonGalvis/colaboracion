bbcb1c3c844dc326cac9a9d745601d1f900a9c07
JheysonGalvis <tecnojheyson@gmail.com>
2025-05-08 21:03:57 -0500

    Abordamos hasta la página 6 del pdf de variables

---
+++ grupo_0/variables.py
@@ -0,0 +1,18 @@
+# Ejemplo básico de uso de print
+mensaje = "Hola, mundo!"
+print(mensaje)
+
+# Formato de cadenas página 3   
+nombre = "Aurelio Cheveroni"
+edad = 70 
+print(f"Mi nombre es {nombre} y tengo {edad} años")         
+
+# Página 5 Fin de línea
+print("Hola", end=" ") 
+print("Mundo")
+
+# Sintaxis página 6
+# Especificando un separador diferente
+nombre = "Mery"
+edad = 25     
+print(nombre, edad, sep="-")
