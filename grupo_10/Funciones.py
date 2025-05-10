93ecb3f59d905967c26e49603c25f788e9a78670
Sofiavelasco177 <velascosantamariaa1105546@gmail.com>
2025-05-09 20:08:07 -0500

    termine ejercicios

--- grupo_10/Funciones.py
+++ grupo_10/Funciones.py
@@ -26,3 +26,33 @@ def celsius_a_fahrenheit(celsius):
 
 temperatura_celsius=25
 print(celsius_a_fahrenheit(temperatura_celsius))
+
+
+def es_primo(numero):
+    if numero < 2:
+        return False
+    for i in range(2, int(numero ** 0.5) + 1):
+        if numero  %i == 0:
+
+            return False
+
+        return True
+
+numero_primo = 17
+print(es_primo(numero_primo))
+
+
+def suma_cuadrados(a,b) :
+    return a**2 + b**2
+
+numero1 = 3
+numero2 = 4
+
+print (suma_cuadrados(numero1, numero2))
+
+
+def saludar (nombre):
+    return f"Hola, {nombre} !"
+
+nombre_usuario = "Juan"
+print(saludar(nombre_usuario))
